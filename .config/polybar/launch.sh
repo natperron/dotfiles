ab98a0fa86171cafc1f6aaae797024d48b25a08b
Natoune <natou022@gmail.com>
2021-07-14 11:38:40 -0400

    Update polybar and use picom

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,18 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 killall -q polybar
 
-# while pgrep -x polybar >/dev/null; do sleep 1; done
+MONITOR=DP-5 polybar main & 
+MONITOR=DP-2 polybar primary &
 
-# if type "xrandr"; then
-#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#     MONITOR=$m polybar --reload example &
-#   done
-# else
-#   polybar --reload main &
-
-# fi
-
-MONITOR=HDMI-1 polybar main & 
-MONITOR=DP-2 polybar main & 
-MONITOR=eDP-1 polybar primary &

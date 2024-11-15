5cc9cfe1b51acc2a15a133c63ad4a5493eb53d36
Nathan Perron <nathan.perron22@gmail.com>
2024-11-15 15:13:38 -0500

    Fix eww css, add delay for open-tray and fix postswitch

--- .config/eww/scripts/open-tray.sh
+++ .config/eww/scripts/open-tray.sh
@@ -14,7 +14,14 @@ hide_tray() {
 
 if [[ $1 == false ]]; then
     show_tray
-    i3-msg -t subscribe '["window"]' > /dev/null && hide_tray
+    while i3-msg -t subscribe '["window"]' > /dev/null; do
+        sleep 0.5
+        xpropid=$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)
+        [[ "$xpropid" == "0x0" || "$xpropid" == "" ]] && break
+        wm_class=$(xprop -id "$xpropid" WM_CLASS | awk '{print $NF}' | tr -d '"')
+        [[ "$wm_class" != "stalonetray" ]] && break
+    done
+    hide_tray
 else
     hide_tray
 fi
